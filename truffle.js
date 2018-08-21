35918703b95e15a45997cbe71a1a9b8bd2c02ae7
Sunjin Lee <styner32@gmail.com>
2018-08-21 18:35:20 +0900

    remove unused action

--- truffle.js
+++ truffle.js
@@ -5,8 +5,7 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*',
-      from: '0x8343e588e14fccb50e7d1b4d88853ed7b0937e33'
+      network_id: '*'
     }
   }
 };
